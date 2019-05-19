6e266b33e3547554ce41c229dd53fdc1f01e74b0
jeremy <xiaowe2016@gmail.com>
2019-05-19 10:51:06 +0800

    delete loading

--- js/resize-v=1.0.0.js
+++ js/resize-v=1.0.0.js
@@ -14,4 +14,25 @@ window.addEventListener('load', function () {
         wid = $('#programme .detail').width();
         $('#programme .detail .tab-cont div').css({maxWidth: wid+'px'})
     })
+
+    var count = 0;
+    var outTime=5;//分钟
+    window.setInterval(go, 1000);
+    function go() {
+        count++;
+        if (count == outTime*60) {
+             window.location.reload();
+        }
+    }
+    var x ;
+    var y ;
+    //监听鼠标
+    document.onmousemove = function (event) {
+            count = 0;
+    };
+    //监听键盘
+    document.onkeydown = function () {
+            count = 0;
+    };
+
 });
